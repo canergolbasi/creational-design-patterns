4862bdd41a5c2917c37fe360e9bf9330c8cee1eb
canergolbasi <canergolbasi@gmail.com>
2022-11-17 15:41:58 +0300

    Builder

---
+++ builder/src/builder/Builder.java
@@ -0,0 +1,13 @@
+package builder;
+
+public interface Builder {
+
+	Builder setCeilingHeight(int ceilingHeight);
+
+	Builder setFloorNumber(int floorNumber);
+
+	Builder setNumberOfWindows(int numberOfWindows);
+
+	Builder setNumberOfDoors(int numberOfDoors);
+
+}
